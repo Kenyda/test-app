c968cb00022c87a8ef818a37f426b641605b7276
Keny <kenyda25@gmail.com>
2022-01-14 10:56:46 +0300

    Edit loading

--- src/components/UI/Loading.tsx
+++ src/components/UI/Loading.tsx
@@ -1,21 +1,17 @@
 import React from "react";
-import { Box, CircularProgress, Container, Typography } from "@mui/material";
+import {Box, CircularProgress, Container, Grid, Typography} from "@mui/material";
 
 const Loading = (props: { loadingText: string }) => {
   return (
-    <Box sx={{ position: "relative", display: "block" }}>
-      <Box
-        sx={{
-          margin: "0 auto",
-          display: "block",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <CircularProgress />
+    <Grid
+      container
+      direction="row"
+      justifyContent="space-around">
+      <Grid item>
+        <CircularProgress sx={{ml: 2}} />
         <Typography>{props.loadingText}</Typography>
-      </Box>
-    </Box>
+      </Grid>
+    </Grid>
   );
 };
 
